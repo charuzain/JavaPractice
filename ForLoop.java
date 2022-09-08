ecc49aa13d1a20aac1b77c6992e2dcba777b7d7f
charuzain <charuabhizain@gmail.com>
2022-09-07 20:12:24 -0400

    for loop in java

---
+++ ForLoop.java
@@ -0,0 +1,9 @@
+public class ForLoop {
+  public static void main(String[] args) {
+    int sum = 0;
+    for (int i = 0; i < 10; i++) {
+      sum += i;
+    }
+  System.out.println("Sum of first 10 number is " + sum);
+  }
+}
